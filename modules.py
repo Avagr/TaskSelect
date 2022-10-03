eacf64552668418f849ed3fbe42f20075a9c1a5e
Avagr <sasha.agroskin@gmail.com>
2022-10-03 16:56:04 +0300

    Add bottom-up top-down model

--- modules.py
+++ modules.py
@@ -3,7 +3,7 @@ import math
 import torch
 from torch import nn
 from transformers import ViTModel, ViTConfig, ViTForImageClassification
-from transformers.models.vit.modeling_vit import ViTAttention
+from transformers.models.vit.modeling_vit import ViTAttention, ViTEmbeddings
 
 
 class TaskEmbeddings(nn.Module):
@@ -71,7 +71,7 @@ class TaskDecoderBlock(nn.Module):
     def __init__(self, enc_config: ViTConfig, dec_config: ViTConfig):
         super().__init__()
         self.chunk_size_feed_forward = dec_config.chunk_size_feed_forward
-        self.seq_len_dim = 1
+
         self.attention = ViTAttention(dec_config)
         self.cross_attention = CrossAttention(enc_config, dec_config)
         self.feedforward = nn.Sequential(nn.Linear(in_features=dec_config.hidden_size,
@@ -129,6 +129,228 @@ class LeftRightEncDec(nn.Module):
             for block in self.decoder_blocks:
                 decoder_state = block(decoder_state, self.layernorm(encoder_hidden_states[-1]))
         return self.classifier(decoder_state[:, 0, :])
+
+
+class LateralAttention(nn.Module):
+    """
+    Partly taken from huggingface: https://huggingface.co/transformers/v4.8.2/_modules/transformers/models/vit/modeling_vit.html
+    """
+
+    def __init__(self, config: ViTConfig):
+        super().__init__()
+        self.num_attention_heads = config.num_attention_heads
+        self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query = nn.Linear(in_features=config.hidden_size, out_features=self.all_head_size)
+        self.key = nn.Linear(in_features=config.hidden_size, out_features=self.all_head_size)
+        self.value = nn.Linear(in_features=config.hidden_size, out_features=self.all_head_size)
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(self, hidden_states, lateral_states, mix_with):
+        key_layer = self.transpose_for_scores(self.key(hidden_states))
+        value_layer = self.transpose_for_scores(self.value(hidden_states))
+        query_layer = self.transpose_for_scores(self.query(hidden_states))
+
+        if mix_with == 'q':
+            query_layer = query_layer * lateral_states
+        elif mix_with == 'k':
+            key_layer = key_layer * lateral_states
+        elif mix_with == 'v':
+            value_layer = value_layer * lateral_states
+        else:
+            raise ValueError("Only 'q', 'k', 'v' values are permitted for lateral connections")
+
+        # Take the dot product between "query" and "key" to get the raw attention scores.
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+
+        # Normalize the attention scores to probabilities.
+        attention_probs = self.softmax(attention_scores)
+
+        context_layer = torch.matmul(attention_probs, value_layer)
+
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+
+        return context_layer
+
+
+class SelfAttention(nn.Module):
+    """
+    Partly taken from huggingface: https://huggingface.co/transformers/v4.8.2/_modules/transformers/models/vit/modeling_vit.html
+    """
+
+    def __init__(self, config: ViTConfig):
+        super().__init__()
+        self.num_attention_heads = config.num_attention_heads
+        self.attention_head_size = int(config.hidden_size / config.num_attention_heads)
+        self.all_head_size = self.num_attention_heads * self.attention_head_size
+
+        self.query = nn.Linear(in_features=config.hidden_size, out_features=self.all_head_size)
+        self.key = nn.Linear(in_features=config.hidden_size, out_features=self.all_head_size)
+        self.value = nn.Linear(in_features=config.hidden_size, out_features=self.all_head_size)
+
+        self.softmax = nn.Softmax(dim=-1)
+
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.num_attention_heads, self.attention_head_size)
+        x = x.view(*new_x_shape)
+        return x.permute(0, 2, 1, 3)
+
+    def forward(self, hidden_states, return_state):
+        key_layer = self.transpose_for_scores(self.key(hidden_states))
+        value_layer = self.transpose_for_scores(self.value(hidden_states))
+        query_layer = self.transpose_for_scores(self.query(hidden_states))
+
+        if return_state == 'q':
+            return_layer = query_layer
+        elif return_state == 'k':
+            return_layer = key_layer
+        elif return_state == 'v':
+            return_layer = value_layer
+        else:
+            raise ValueError("Only 'q', 'k', 'v' values are permitted for lateral connections")
+
+        # Take the dot product between "query" and "key" to get the raw attention scores.
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+
+        attention_scores = attention_scores / math.sqrt(self.attention_head_size)
+
+        # Normalize the attention scores to probabilities.
+        attention_probs = self.softmax(attention_scores)
+
+        context_layer = torch.matmul(attention_probs, value_layer)
+
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.all_head_size,)
+        context_layer = context_layer.view(*new_context_layer_shape)
+
+        return context_layer, return_layer
+
+
+class LateralEncoderBlock(nn.Module):
+    """
+        Partly taken from huggingface: https://huggingface.co/transformers/v4.8.2/_modules/transformers/models/vit/modeling_vit.html
+        """
+
+    def __init__(self, config: ViTConfig, mix_with: str, use_self_attention: bool):
+        super().__init__()
+        self.chunk_size_feed_forward = config.chunk_size_feed_forward
+
+        self.use_self_attention = use_self_attention
+        self.mix_with = mix_with
+        if use_self_attention:
+            self.attention = ViTAttention(config)
+        self.lateral_attention = LateralAttention(config)
+        self.feedforward = nn.Sequential(nn.Linear(in_features=config.hidden_size,
+                                                   out_features=config.intermediate_size),
+                                         nn.GELU(),
+                                         nn.Linear(in_features=config.intermediate_size,
+                                                   out_features=config.hidden_size))
+        self.layernorm_before = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        if use_self_attention:
+            self.encoder_layernorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.lateral_layernorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.layernorm_after = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+
+    def forward(self, hidden_states, lateral_states):
+        hidden_states = self.layernorm_before(hidden_states)
+        if self.use_self_attention:
+            self_attention_output = self.attention(hidden_states)[0]
+            hidden_states = self.encoder_layernorm(self_attention_output + hidden_states)
+
+        lateral_attention_outputs = self.lateral_attention(hidden_states,
+                                                           lateral_states,
+                                                           self.mix_with)
+
+        hidden_states = lateral_attention_outputs + hidden_states
+
+        layer_output = self.layernorm_after(hidden_states)
+        feedforward_output = self.feedforward(layer_output)
+        feedforward_output = hidden_states + feedforward_output
+
+        return feedforward_output
+
+
+class TransparentEncoderBlock(nn.Module):
+    """
+        Partly taken from huggingface: https://huggingface.co/transformers/v4.8.2/_modules/transformers/models/vit/modeling_vit.html
+        """
+
+    def __init__(self, config: ViTConfig, return_state: str):
+        super().__init__()
+        self.chunk_size_feed_forward = config.chunk_size_feed_forward
+        self.attention = SelfAttention(config)
+        self.return_state = return_state
+        self.feedforward = nn.Sequential(nn.Linear(in_features=config.hidden_size,
+                                                   out_features=config.intermediate_size),
+                                         nn.GELU(),
+                                         nn.Linear(in_features=config.intermediate_size,
+                                                   out_features=config.hidden_size))
+        self.layernorm_before = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.layernorm_after = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+
+    def forward(self, hidden_states):
+        hidden_states = self.layernorm_before(hidden_states)
+
+        lateral_attention_outputs, attention_state = self.attention(hidden_states, self.return_state)
+
+        hidden_states = lateral_attention_outputs + hidden_states
+
+        layer_output = self.layernorm_after(hidden_states)
+        feedforward_output = self.feedforward(layer_output)
+        feedforward_output = hidden_states + feedforward_output
+
+        return feedforward_output, attention_state
+
+
+class LeftRightBUTD(nn.Module):
+
+    def __init__(self, num_classes, config: ViTConfig, total_token_size, use_self_attention, mix_with):
+        super().__init__()
+        self.task_encoder_blocks = nn.ModuleList(
+            [TransparentEncoderBlock(config, mix_with) for _ in range(config.num_hidden_layers)])
+        self.image_embeddings = ViTEmbeddings(config)
+        self.image_encoder_blocks = nn.ModuleList(
+            [LateralEncoderBlock(config, mix_with, use_self_attention) for _ in range(config.num_hidden_layers)])
+        self.task_embeddings = nn.Linear(in_features=2, out_features=config.hidden_size)
+        self.argument_embeddings = nn.Linear(in_features=num_classes, out_features=config.hidden_size)
+        self.relu = nn.ReLU()
+        self.stretch_embedding = nn.Linear(in_features=2 * config.hidden_size, out_features=total_token_size)
+        self.layernorm = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
+        self.task_position_embeddings = nn.Parameter(
+            torch.zeros(1, total_token_size // config.hidden_size, config.hidden_size))
+        self.classifier = nn.Linear(in_features=config.hidden_size, out_features=num_classes)
+
+    def forward(self, img: torch.Tensor, task: torch.FloatTensor, argument: torch.FloatTensor) -> torch.Tensor:
+
+        embedded_image = self.image_embeddings(img)
+
+        task_tokens = self.task_embeddings(task)
+        argument_tokens = self.argument_embeddings(argument)
+
+        task_input = self.stretch_embedding(self.relu(torch.cat((task_tokens, argument_tokens), dim=1))).view(
+            *embedded_image.shape) + self.task_position_embeddings
+
+        mixing_layers = []
+
+        for block in self.task_encoder_blocks:
+            task_input, mixing_state = block(task_input)
+            mixing_layers.append(mixing_state)
+
+        for mixing_state, block in zip(mixing_layers[::-1], self.image_encoder_blocks):
+            embedded_image = block(embedded_image, mixing_state)
+
+        return self.classifier(self.layernorm(embedded_image[:, 0, :]))
 
 
 class LeftRightEncoder(nn.Module):
